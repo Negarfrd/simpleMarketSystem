6adfdda6190753adc5e469297d9121fcb42c8790
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-09-13 17:43:48 +0330

    3rd commit. item subclasses added.

---
+++ Models/Clothes.cs
@@ -0,0 +1,31 @@
+﻿namespace DotNetHW2;
+
+public class Clothes : Item
+{
+    private string _color { get; }
+    public Clothes(string name, double price, string color) : base(name, price)
+    {
+        this._color = color;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Models/Shoes.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetHW2;
+
+public class Shoes : Item
+{
+    private int _size { get; }
+
+    public Shoes(string name, double price, int size) : base(name, price)
+    {
+        _size = size;
+    }
+}
