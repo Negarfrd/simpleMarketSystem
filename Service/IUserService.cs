a89bd9edfb4a30343197b4ffd5d57a0574a2e6a3
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-09-13 22:13:09 +0330

    8th commit. interface and service class for user methods added.

---
+++ Service/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace Service;
+
+public interface IUserService
+{
+    public string Login(string username, string password);
+    public string Signup(string username, string password);
+    public string ChangeUsername(string username, string newUsername);
+    public string ChangePassword(string oldPassword, string newPassword);
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,24 @@
+﻿namespace Service;
+
+public class UserService : IUserService
+{
+    public string Login(string username, string password)
+    {
+        
+    }
+
+    public string Signup(string username, string password)
+    {
+       
+    }
+
+    public string ChangeUsername(string username, string newUsername)
+    {
+        
+    }
+
+    public string ChangePassword(string oldPassword, string newPassword)
+    {
+        
+    }
+}
