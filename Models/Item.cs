b59bdc4476e0630e07cb84bceda03f5bd1953bfd
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-09-13 17:11:18 +0330

    2nd commit. item class properties added.

--- Models/Item.cs
+++ Models/Item.cs
@@ -2,4 +2,12 @@
 
 public class Item
 {
+    private string name { get; set; }
+    private double price { get; set; }
+
+    public Item(string name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
 }
