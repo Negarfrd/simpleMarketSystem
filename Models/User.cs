d0fe9c66c93a160263b50cc1ffdcf57aebe7d271
Negar.Fereidouni <negar.fereidouni96@gmail.com>
2024-09-13 17:50:58 +0330

    4th commit. shopping list property added to user class.

--- Models/User.cs
+++ Models/User.cs
@@ -4,10 +4,12 @@ public class User
 {
     private string username { get; set; }
     private string password { get; set; }
+    private Item[] shoppingList { get; }
 
     public User(string username, string password)
     {
         this.username = username;
         this.password = password;
+        this.shoppingList = new Item[5];
     }
 }
